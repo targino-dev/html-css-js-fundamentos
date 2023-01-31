d30d59f216f6df6f67d31b9e57999b2e493c82a7
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-01-30 23:05:51 -0300

    tipos de dados

---
+++ exercicios-fixacao/5-tipos-de-dados/script.js
@@ -0,0 +1,5 @@
+var fruta = "Banana";
+var preco = 3.50;
+var levar = true;
+var novoValor;
+alert(fruta + " " + preco + " " + levar + " " + novoValor); // Banana 3.50 true novoValor(Undefined - Não definido)
