8b5bfa41c71ad4c541929322ad95bb289a666071
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-01-31 21:14:55 -0300

    entrada soma valores

---
+++ exercicios-fixacao/8-entrada-soma-valores/script.js
@@ -0,0 +1,7 @@
+// Lê os números
+var num1 = Number(prompt("1º Número: "));
+var num2 = Number(prompt("2º Número: "));
+// Calcula a soma
+var soma = num1 + num2;
+// Exibe o resultado
+alert("Soma é: " + soma);
