f5c80a0cbb9a494eec6fa7fde89347e5c3036e97
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-01 22:09:31 -0300

    calculo tempo viagem

---
+++ exercicios-fixacao/10-calculo-tempo-viagem/script.js
@@ -0,0 +1,7 @@
+// Lê os dados de entrada
+var dias = Number(prompt("Nº de dias: "));
+var horas = Number(prompt("Nº de horas: "));
+// Calcula a duraçao
+var total = (dias * 24) + horas;
+// Exibe o total
+alert("Total de horas: " + total);
