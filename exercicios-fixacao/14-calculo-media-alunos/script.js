8f35c147f48e58a4d2a349b17da7340276b2abac
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-04 04:15:30 -0300

    calculo media alunos

---
+++ exercicios-fixacao/14-calculo-media-alunos/script.js
@@ -0,0 +1,7 @@
+// Lê as duas notas
+let nota1 = Number(prompt("Qual a primeira nota? "));
+let nota2 = Number(prompt("Qual a segunda nota? "));
+// Calcula a média
+let media = (nota1 + nota2) / 2;
+// Mostra o resultado
+alert("1ª Nota: " + nota1.toFixed(1) + "\n2ª Nota: " + nota2.toFixed(1) + "\nMédia: " + media.toFixed(1));
