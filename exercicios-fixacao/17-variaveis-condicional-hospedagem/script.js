2b8990a24a2653473f6d032dc092ffd79c89424a
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-05 21:03:24 -0300

    variaveis condicional hospedagem

---
+++ exercicios-fixacao/17-variaveis-condicional-hospedagem/script.js
@@ -0,0 +1,8 @@
+// Pop de Confirmação
+let confirma = (confirm("Seja bem vindo(a) aventureiro(a)! \nGostaria de pagar R$ 50,00 para passar a noite em nossa hospedagem? "));
+// Exiba mensagem - Condicional
+if (confirma == true) {
+    alert("Otimo!! Nós temos as melhores camas de toda a região!");
+} else {
+    alert("Que pena! Você parecia ser uma pessoa legal!");
+}
