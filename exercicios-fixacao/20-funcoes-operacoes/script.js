eb1aa55dfd56f19284265ca9f21075076448b8c9
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-06 20:11:17 -0300

    funcoes operacoes

---
+++ exercicios-fixacao/20-funcoes-operacoes/script.js
@@ -0,0 +1,43 @@
+// Criar funções para operações matemáticas
+function somar(numero1, numero2) {
+    let soma = numero1 + numero2;
+    return "A soma dos dois números é " + soma;
+}
+
+console.log(somar(2, 5));
+
+function subtrair(numero1, numero2) {
+    let subtracao = numero1 - numero2;
+    return "A soma dos dois números é " + subtracao;
+}
+
+console.log(subtrair(5, 2));
+
+function multiplicar(numero1, numero2) {
+    let multiplicacao = numero1 * numero2;
+    return "A multiplicacao dos dois numero é " + multiplicacao;
+}
+
+console.log(multiplicar(5, 2));
+
+
+function dividir(numero1, numero2) {
+    let divisao = numero1 / numero2;
+    return "A multiplicacao dos dois numero é " + divisao;
+}
+
+console.log(dividir(10, 2));
+
+
+function resto(numero1, numero2) {
+    let restou = numero1 % numero2;
+    return "O resto da divisão dos dois numero é " + restou;
+}
+
+console.log(resto(10, 3));
+
+
+
+
+
+
