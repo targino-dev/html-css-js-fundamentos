2cfc433dd46749a92dd3ad1073bf0717690dea0c
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-05 21:01:35 -0300

    tipos condicional boleto

---
+++ exercicios-fixacao/16-tipos-condicional-boleto/script.js
@@ -0,0 +1,8 @@
+// Variável se o boleto está pago
+let boletoPago = false;
+// Condicional se o boleto está pago - Estrutura If/Else
+if (boletoPago == true) {
+    alert("O boleto está pago!");
+} else {
+    alert("O boleto não está pago!");
+}
