18499f3cb8dac3ddc12052c9aaf1165827e8369c
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-18 22:18:17 -0300

    hoisting

---
+++ exercicios-fixacao/21-hoisting/script.js
@@ -0,0 +1,11 @@
+var pais // declaração.
+pais = "Brasil"; // atribuição.
+
+console.log(estado);
+var estado = "Rio de Janeiro"
+
+console.log(cidade); // Vai dar undefined, porque a declaracao da variável sobe, mas não sobe a atribuição.
+var cidade = "Rio de Janeiro" // declaracao e atribuicao.
+
+console.log (bairro); // Com let ocorrerá o erro de indefinição da variável, nem a declaração desta sobe.
+const bairro = "Rocha Miranda"; // declaração e atribuição.
