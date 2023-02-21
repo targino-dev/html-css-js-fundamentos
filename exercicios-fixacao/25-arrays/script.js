febc06e6f8adb3a41d0d05e56c99558df7d7b4cd
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-20 23:25:36 -0300

    arrays

---
+++ exercicios-fixacao/25-arrays/script.js
@@ -0,0 +1,7 @@
+let filmes = ["Matrix", "Batman"]; // Para criar array é utilizado os colchetes
+
+console.log(filmes[0]); // O conteudo é mostrado a partir do nome da variavel + [], iniciando do 0 (zero)
+
+let descricaoFilmes = ["Ninja", 5, true];
+
+console.log(descricaoFilmes);
