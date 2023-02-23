a0998fcd8f0f67ea4b4e8fcaf8ae9687a6d3bc3b
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-22 22:21:49 -0300

    arrays

--- exercicios-fixacao/25-arrays/script.js
+++ exercicios-fixacao/25-arrays/script.js
@@ -1,3 +1,4 @@
+/*
 let filmes = ["Matrix", "Batman"]; // Para criar array é utilizado os colchetes
 
 console.log(filmes[0]); // O conteudo é mostrado a partir do nome da variavel + [], iniciando do 0 (zero)
@@ -5,3 +6,38 @@ console.log(filmes[0]); // O conteudo é mostrado a partir do nome da variavel +
 let descricaoFilmes = ["Ninja", 5, true];
 
 console.log(descricaoFilmes);
+ // Arrays
+
+let filmes = [
+    ["Batman", 5, true];
+    ["Matrix", 5, true];
+]
+
+console.log(filmes); // Retorna o array com os 2 arrays embutidos
+console.log(filmes[0][0]); // Retorna o nome do Batman
+console.log(filmes[1][0]); // Retorna o nome Matrix
+
+
+let frutas = ["maca", "pera", "morango", "laranja"];
+let frutasTamanho = frutas.length // para saber a quantidade
+
+
+for (let i = 0; i < frutasTamanho; i++) {
+    console.log("Fruta: " + frutas[i])
+}  // Com laço for
+
+let frutas = ["maca", "pera", "morango", "laranja"];
+
+frutas.forEach(function(item) {  // .forEach = Busca cada elemento do Array
+    console.log("Fruta: " + item); 
+});  */
+
+let frutas = ["maca", "pera", "morango", "laranja"];
+
+frutas.push("manga"); // .push Adiciona elementos no final do array
+frutas.pop(); // .pop Retira o ultimo elemento do array
+frutas.shift(); // .shift Remove o primeiro elemento do array
+frutas.unshift("pessego"); // .unshift Adiciona elementos no inicio do array
+
+let posicaoMorange = frutas.indexOf("morango");
+console.log(posicaoMorango);
