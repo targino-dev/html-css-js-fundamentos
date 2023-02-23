02f2a5622d5bc17d85a5fe56cfc3195db19f2b6c
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-23 20:14:22 -0300

    objetos

---
+++ exercicios-fixacao/26-objetos/script.js
@@ -0,0 +1,12 @@
+let tenis = { // Para criar um objeto, é necessário a utilização de chaves {conteúdo}
+    cor: "branco",  // chave: valor
+    tamanho: "42",
+    emEstoque: true,
+}
+
+tenis.valor = 200 // Atribui uma nova caracterítica ao objeto.
+
+console.log(tenis); // Vai mostrar um Object, com as propriedades e valores internos
+console.log(tenis.cor); // Para verificar a propriedade do objeto
+console.log(tenis["cor"]); // Forma alternativa de poder visualizar a propriedade
+
