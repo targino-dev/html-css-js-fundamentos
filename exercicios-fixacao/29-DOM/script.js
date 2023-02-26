6998544a15357b6734b8ebf931208a18421686c8
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-26 01:17:22 -0300

    dom

---
+++ exercicios-fixacao/29-DOM/script.js
@@ -0,0 +1,18 @@
+//  console.log(document); // Vai aparecer os elementos da página
+
+// let p = document.getElementById("paragrafo1");
+
+// console.log(p);
+
+// let inputsPorClasse = document.getElementsByClassName("tag-de-input"); // Para selecionar a classe dos dois inputs
+
+// console.log(inputsPorClasse[0]) // Para selecionar o primeiro input
+
+// let inputPoTag = document.getElementsByTagName("input");
+
+// console.log(inputPoPag);
+
+// let inputPorName = document.querySelectorAll("input[name='email']"); // Vai selecionar o que possui name="email"
+
+// console.log(inputPorName[1]); // Vai mostrar o segundo input
+
