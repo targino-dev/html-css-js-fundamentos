d19af2f60e59a0ddcbe53ce94f595bb128fc8d89
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-02-28 22:03:44 -0300

    manipulando o dom ii

--- exercicios-fixacao/31-manipulando-o-DOM-II/script.js
+++ exercicios-fixacao/31-manipulando-o-DOM-II/script.js
@@ -1,7 +1,29 @@
 function alterarCorDeFundoPrimeiroPost() {
+    let posts = document.getElementsByClassName("post");
 
-    console.log(posts)
+    console.log(posts);
 
-    let primeiroPost = post[0];
-    primeiroPost.style.backgroundColor = "red"
-} // Parei aqui!
+    let primeiroPost = post[0]; // vai 
+    console.log("primeiroPost", primeiroPost);
+    primeiroPost.style.backgroundColor = "red";  // Para ativar o background, é necessario o camelcase!
+}
+
+function aumentarFonteSegundoPost() {
+    let textPosts = document.getElementsByClassName ("texto-post"); // Seleciona a class texto-post
+
+    textoPost[1].classList.add("fonte-grande"); // Vai adicionar uma nova classe ao texto-post 2
+    
+    console.log(textoPost[1].classList);
+}
+
+function marcarRadio(genero) {
+    let radioMasculino = document.getElementById("genero-masculino"); // Seleciona o elemento masculino
+
+    let radioFeminino = document.getElementById("genero-feminino"); // Seleciona o elemento feminino
+
+    if(genero === "M") {
+        radioMasculino.checked = true; // Marca como checked o primeiro radio button
+    } else if(genero === "F") {
+        radioFeminino.checked = true; // Marca como checked o segundo radio button
+    }
+}
