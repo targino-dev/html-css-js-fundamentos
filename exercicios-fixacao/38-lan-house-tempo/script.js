c963132697a804a7c6836846d295b895b28f14ca
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-03-15 22:02:14 -0300

    lan house tempo

--- exercicios-fixacao/38-lan-house-tempo/script.js
+++ exercicios-fixacao/38-lan-house-tempo/script.js
@@ -1,15 +1,16 @@
 function calcularValor() {
     let inValor = document.getElementById("inValor"); // Seleciona o campo do valor
     let inTempo = document.getElementById("inTempo"); // Seleciona o tempo do campo
-    let outValor = document.getElementById("outValor"); // Seleciona o titulo para saída do nome do medicamento
+    let outValor = document.getElementById("outValor"); // Seleciona o titulo para saída do valor
 
     let valor = Number(inValor.value); // Transforma o valor de 15min em um numero
     let tempo = Number(inTempo.value); // Transforma o tempo em numero
 
-    let calculo = tempo / valor; // 
-    outNome.textContent = "Promoção de " + medicamento; // Mostra no titulo outNome o nome do medicamento
-    outPromocao.textContent = "Leve 2 por apenas R$: " + promocao.toFixed(2); // Mostra no titulo outPromocao a promocao
+    let calculo = tempo / 15; // Divide o tempo total em frações de 15 minutos
+    let precoTempo = calculo * valor; // Calcula o valor do tempo de 15 minutos pelo resultado da divisao por 15
+
+  outValor.textContent = "Valor a Pagar R$: " + precoTempo.toFixed(2); // Mostra no titulo outValor o custo
 }
 
 let btCalcular = document.getElementById("btCalcular"); // Seleciona o botão
-btCalcular.addEventListener("click", calcularPromocao); // Dispara a funcao calcularPromocao com o clique
+btCalcular.addEventListener("click", calcularValor); // Dispara a funcao calcularPromocao com o clique
