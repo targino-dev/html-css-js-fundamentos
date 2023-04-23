53832b36b06637539e185d642d7aab7c768a4f8a
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-22 22:37:01 -0300

    calcular peso

--- exercicios-fixacao/44-calcular-peso/src/js/script.js
+++ exercicios-fixacao/44-calcular-peso/src/js/script.js
@@ -1,42 +1,57 @@
 function calcularPeso() {
-    // Cria referência aos elementos manipulados pela function
-    let inNome = document.getElementById("inNome");
-    let rbMasculino = document.getElementById("rbMasculino");
-    let rbFeminino = document.getElementsById("rbFeminino");
-    let inAltura = document.getElementById("inAltura");
-    let outResposta = document.getElementById("outResposta");
 
-    // Obtém o conteúdo dos campos de edição da página
-    let nome = inNome.value;
-    let masculino = rbMasculino.checked;
-    let feminino = rbFeminino.checked;
-    let altura = Number(inAltura.value)
+    // cria referência aos elementos manipulados pela function
+    var inNome = document.getElementById("inNome");
+    var rbMasculino = document.getElementById("rbMasculino");
+    var rbFeminino = document.getElementById("rbFeminino");
+    var inAltura = document.getElementById("inAltura");
+    var outResposta = document.getElementById("outResposta");
 
-    // Verifica se nome foi preenchido e sexo selecionado
+    // obtém o conteúdo dos campos de edição da página
+    var nome = inNome.value;
+    var masculino = rbMasculino.checked;
+    var feminino = rbFeminino.checked;
+    var altura = Number(inAltura.value);
+
+    // verifica se nome foi preenchido e sexo selecionado
     if (nome == "" || (masculino == false && feminino == false)) {
-        alert("Por favor, informe o nome e selecione o sexo...");
-        inNome.focus(); // Coloca o foco no campo de edição para digitar inNome
-        return;
+    alert("Por favor, informe o nome e selecione o sexo...");
+    inNome.focus(); // posiciona (joga o foco) no campo de edição inNome
+    return;
     }
 
-    // Se altura vazio (0) ou NaN: Not-a-Number (um texto for informado, por exemplo)
+    // se altura vazio (0) ou NaN: Not-a-Number (um texto for informado, por exemplo)
     if (altura == 0 || isNaN(altura)) {
-        alert("Por favor, informe a altura corretamente...");
-        inAltura.focus();
-        return;
+    alert("Por favor, informe a altura corretamente...");
+    inAltura.focus();
+    return;
     }
 
-    // Se masculino (significa se masculino == true)
+    // se masculino (significa se masculino == true)
     if (masculino) {
-        let peso = 22 * Math.pow(altura, 2); // Math.pow eleva ao quadrado
+    var peso = 22 * Math.pow(altura, 2); // Math.pow eleva ao quadrado
     } else {
-        let peso = 21 * Math.pow(altura, 2); 
+    var peso = 21 * Math.pow(altura, 2);
     }
 
-    // Apresenta a resposta ( altera o conteúdo da linha outResposta)
-    outResposta.textContent = nome + ": Seu peso ideal é " + peso.toFixed(3) + "kg";
-}
+    // apresenta a resposta (altera o conteúdo da linha outResposta)
+    outResposta.textContent = nome + ": Seu peso ideal é " +
+    peso.toFixed(3) + " kg";
+    }
 
-// Cria referência ao elemento btCalcular e registra evento associado a calcularPeso
-    let btResultado = document.getElementById("btCalcular");
+    // cria referência ao elemento btCalcular e registra evento associado a calcularPeso
+    var btCalcular = document.getElementById("btCalcular");
     btCalcular.addEventListener("click", calcularPeso);
+
+    function limparCampos() {
+        // Limpa os conteúdos dos elementos
+        document.getElementById("inNome").value = "";
+        document.getElementById("rbMasculino").checked = false;
+        document.getElementById("rbFeminino").checked = false;
+        document.getElementById("inAltura").value = "";
+        document.getElementById("outResposta").textContent = "";
+        // posiciona (joga o foco) no elemento inNome
+        document.getElementById("inNome").focus();
+        }
+        var btLimpar = document.getElementById("btLimpar");
+        btLimpar.addEventListener("click", limparCampos);
