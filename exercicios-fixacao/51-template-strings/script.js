d397a3fde353e20fe3f22cdba0e3d653b4a5dac1
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-25 23:03:01 -0300

    template strings

---
+++ exercicios-fixacao/51-template-strings/script.js
@@ -0,0 +1,14 @@
+// const imprimirInformacoesPessoais = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro nome: " + primeiroNome + ", Último nome: " + ultimoNome + " e Idade: " + idade;
+// }
+
+// console.log(imprimirInformacoesPessoais("Targino", "Nogueira", 29));
+
+const imprimirInformacoesPessoais = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome}, Último nome: ${ultimoNome} e Idade: ${idade}`;
+}
+
+// console.log(imprimirInformacoesPessoais("Targino", "Nogueira", 29));
+console.log(`${imprimirInformacoesPessoais("Targino", "Nogueira", 29)}, ele é um aluno de JS`);
+
+console.log(`Produtividade: ${200 + 800}%!!`);
