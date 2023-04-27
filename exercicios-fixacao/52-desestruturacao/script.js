1ccfc03ca402d4b109d7bdca289e6a98bc36c03c
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-26 23:57:16 -0300

    desestruturacao

---
+++ exercicios-fixacao/52-desestruturacao/script.js
@@ -0,0 +1,25 @@
+// let pessoa = {nome: "Targino", sobrenome: "Nogueira", idade: 29}
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome);
+// console.log(idade);
+
+// Acima, o método padrão para criação de variaveis
+
+// var numeros = ["um", "dois", "tres"]
+
+// let { nome: nome, idade: idade, sobrenome: sobrenome} = pessoa
+
+// console.log(nome)
+// console.log(idade)
+// console.log(sobrenome)
+
+const nuumeros = [1, 2, 3];
+
+const [ um, dois, tres, quatro ] = numeros
+
+console.log(um)
+console.log(dois)
+console.log(tres)
+console.log(quatro)
