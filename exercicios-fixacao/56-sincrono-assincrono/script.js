72df5e13f9fdaedf8bdd69e9a2ddd61a741fc537
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-29 20:36:55 -0300

    sincrono e assincrono

---
+++ exercicios-fixacao/56-sincrono-assincrono/script.js
@@ -0,0 +1,44 @@
+// function colocarAguaParaFerver() {
+//     console.log("Colocar água para ferver");
+// }
+
+// function prepararParaPassarOCafe() {
+//     console.log("Pegar o pó de café");
+//     console.log("Pegar o filtro de café");
+//     console.log("Colocar o café no filtro");
+//     console.log("Colocar o filtro em cima da xícara");
+// }
+
+// function passarOCafe() {
+//     console.log("Passando o café");
+// }
+
+// colocarAguaParaFerver();
+// prepararParaPassarOCafe();
+// passarOCafe();
+
+// // Acima em Sincronia, abaixo de forma Assincrona
+
+// function colocarAguaParaFerver() {
+//     console.log("Colocar água para ferver");
+//         console.log("Agua ferveu")
+//         passarOCafe();
+//     setTimeout(() => {
+
+//     }, 3000)
+// }
+
+// function prepararParaPassarOCafe() {
+//     console.log("Pegar o pó de café");
+//     console.log("Pegar o filtro de café");
+//     console.log("Colocar o café no filtro");
+//     console.log("Colocar o filtro em cima da xícara");
+// }
+
+// function passarOCafe() {
+//     console.log("Passando o café");
+// }
+
+// colocarAguaParaFerver();
+// prepararParaPassarOCafe();
+// passarOCafe();
