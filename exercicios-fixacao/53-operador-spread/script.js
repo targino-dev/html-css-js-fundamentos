e80854e1ddb23c8dcc38da8874c7677c6e9d849e
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-28 23:34:01 -0300

    operador spread

---
+++ exercicios-fixacao/53-operador-spread/script.js
@@ -0,0 +1,19 @@
+const pessoas = ["Pessoa1","Pessoa2","Pessoa3"]
+console.log(pessoas);
+
+// Concatenar dois arrays 
+
+// let grupoPessoas1 = ["Pessoa1","Pessoa2","Pessoa3"]
+// let grupoPessoas2 = ["Pessoa4","Pessoa5","Pessoa6"]
+
+// grupoPessoas1 = grupoPessoas1.concat(grupoPessoas2)
+
+// grupoPessoas1 = [ ...grupoPessoas1, ...grupoPessoas2]
+
+// Clonar Objeto
+
+// const grupoPessoas1 = {nome: "Pessoa1", idade: 30}
+
+// const objetoClonado = {...grupoPessoas1} // Vai clonar o objeto acima para a variavel objetoClonado
+// console.log(grupoPessoas1);
+// console.log(objetoClonado);
