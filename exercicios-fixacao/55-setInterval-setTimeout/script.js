217ac79b52b9ec046434b12b0c24ce1f9d9ee390
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-04-29 20:34:03 -0300

    setInterval e setTimeout

---
+++ exercicios-fixacao/55-setInterval-setTimeout/script.js
@@ -0,0 +1,8 @@
+// setTimeout(function(){
+//     alert("Olá mundo!");
+// }, 3000) // Esse 3000 é milisegundos, coloca um temporizador para executar o escopo da função
+
+// setInterval(function() {
+//     alert("O setInterval ao invés do Timeout, executa a cada intervalo de tempo, nesse caso em 3 segundos");
+// }, 3000)
+
