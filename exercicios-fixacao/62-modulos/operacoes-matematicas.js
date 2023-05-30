8d19632ecd688d3f9240793613374ff2e812a22a
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-05-29 22:55:08 -0300

    modulos

---
+++ exercicios-fixacao/62-modulos/operacoes-matematicas.js
@@ -0,0 +1,12 @@
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+// Pode utilizar o elemento export na frente do nome da função para chama-la de forma unitária
+// Mas, se deseja que todas as funções sejam chamadas utilize o modelo abaixo
+
+export { somar, multiplicar } // Export { nome das funções separadas por vírgulas}
