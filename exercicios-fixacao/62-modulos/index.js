a43c7131f0f7d4334ffdf83232635201eccb9135
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-05-29 22:54:34 -0300

    modulos

---
+++ exercicios-fixacao/62-modulos/index.js
@@ -0,0 +1,6 @@
+import {somar, multiplicar} from "./operacoes-matematicas.js" // Por precaução, utilize o .js!
+
+console.log(somar(1, 3));
+console.log(multiplicar(10,1000));
+
+// Tem que colocar type=module no html quando faz a requisição do arquivo.js
