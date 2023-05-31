13bcf01e788da06b13d3117cb395ca749595a5cd
Targino Nogueira <107009616+targino-dev@users.noreply.github.com>
2023-05-31 00:01:22 -0300

    exportacoes

---
+++ exercicios-fixacao/63-exportacoes/index.js
@@ -0,0 +1,8 @@
+import {somar, multiplicar} from "./operacoes-matematicas.js" // Por precaução, utilize o .js!
+
+console.log(somar(1, 3));
+console.log(multiplicar(10,1000));
+
+// Tem que colocar type=module no html quando faz a requisição do arquivo.js
+
+// Para importar um modulo default, só remover as chaves e colocar o nome da função
